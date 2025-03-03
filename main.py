4d36cac0d30ceda2f570de3cc7c883d5b40fb596
AdamJQ1 <ajquan1@asu.edu>
2025-03-02 22:20:42 -0700

    11

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ while True:
     if state == 0:
         my_oled.print_text("test",0,0)
     if state == 1:
-        my_oled.print_text("something else",0,0)
+        my_oled.print_text("something else",0,64)
+    if state == 2:
+        my_oled.oled.line(0,0,10,10,1)
     time.sleep(1)
     state= state +1
     if state == 4:
